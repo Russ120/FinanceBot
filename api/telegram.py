931a90daa70f2a822112129201e6a9ca1acdab00
Rusbel <rusbelrodz@gmail.com>
2025-11-22 22:07:06 -0400

    cambios en el archivo telegram

--- api/telegram.py
+++ api/telegram.py
@@ -1,8 +1,11 @@
-from fastapi import Request
+from fastapi import FastAPI, Request
 from telegram import Update
 from bot import app_tg
 
-async def handler(request: Request):
+app = FastAPI()
+
+@app.post("/api/telegram")
+async def telegram_webhook(request: Request):
     data = await request.json()
     update = Update.de_json(data, app_tg.bot)
     await app_tg.process_update(update)
